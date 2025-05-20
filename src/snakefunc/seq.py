be787616dfccde6adeeaa14378cc843f4d9f7d6c
James Benner <james.benner@gmail.com>
2025-05-19 22:18:36 -0400

    Start refactor to better handle ranges

--- src/snakefunc/seq.py
+++ src/snakefunc/seq.py
@@ -1,10 +1,51 @@
 from collections.abc import Sequence, Callable
-from typing import overload, cast, Any, Self
+from typing import overload, cast, Any, Self, Literal
+
+type RangeType = Literal["range"]
+type AcceptableSequenceType = Literal["bytearray", "bytes", "list", "str", "tuple"]
+type SequenceType = AcceptableSequenceType | RangeType
 
 
 class Seq[T]:
-    def __init__(self, sequence=None) -> None:
+    def __init__(
+        self, sequence: Sequence[T], coerce_range_into: AcceptableSequenceType = "tuple"
+    ) -> None:
         self._value: Sequence[T] = sequence
+        self._coerce_range_into: AcceptableSequenceType = coerce_range_into
+
+    def _coerce_range_value(
+        self, value: Sequence[T]
+    ) -> bytearray | bytes | list[T] | str | tuple[T, ...]:
+        match self._coerce_range_into:
+            case "bytearray":
+                return bytearray(value)
+            case "bytes":
+                return bytes(value)
+            case "list":
+                return list(value)
+            case "str":
+                return "".join(map(str, tuple(value)))
+            case "tuple":
+                return tuple(value)
+            case _:
+                raise ValueError()
+
+    def _get_sequence_type(self) -> SequenceType:
+        match self._value:
+            case bytearray():
+                return "bytearray"
+            case bytes():
+                return "bytes"
+            case list():
+                return "list"
+            case range():
+                return "range"
+            case str():
+                return "str"
+            case tuple():
+                return "tuple"
+            case _:
+                raise ValueError()
 
     @overload
     def filter(self, callback: Callable[[T], bool]) -> Self: ...
@@ -21,8 +62,12 @@ class Seq[T]:
         | Callable[[T, int], bool]
         | Callable[[T, int, Sequence[T]], bool],
     ) -> Self:
+        if self.len() == 0:
+            return self
+
         callback_args: tuple[str, ...] = callback.__code__.co_varnames
         filtered: list[T] = []
+        sequence_type: SequenceType = self._get_sequence_type()
 
         match len(callback_args):
             case 3:
@@ -40,7 +85,19 @@ class Seq[T]:
             case _:
                 raise TypeError
 
-        self._value = filtered
+        match sequence_type:
+            case "bytearray":
+                self._value = bytearray(filtered)
+            case "bytes":
+                self._value = bytes(filtered)
+            case "list":
+                self._value = filtered
+            case "range":
+                self._value = self._coerce_range_value(filtered)
+            case "str":
+                self._value = "".join(filtered)
+            case "tuple":
+                self._value = tuple(filtered)
 
         return self
 
@@ -182,5 +239,7 @@ class Seq[T]:
         return list(self._value)
 
 
-def seq[T](sequence: Sequence[T]) -> Seq[T]:
-    return Seq(sequence)
+def seq[T](
+    sequence: Sequence[T], coerce_range_into: AcceptableSequenceType = "tuple"
+) -> Seq[T]:
+    return Seq(sequence, coerce_range_into)
